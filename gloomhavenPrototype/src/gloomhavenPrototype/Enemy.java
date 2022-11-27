52e6569382a4213eec0436ae231fb6a255a077b4
Kwonishere <118315541+Kwonishere@users.noreply.github.com>
2022-11-27 14:44:33 -0800

    Enemy.java
    Adding Enemy to board

--- gloomhavenPrototype/src/gloomhavenPrototype/Enemy.java
+++ gloomhavenPrototype/src/gloomhavenPrototype/Enemy.java
@@ -1,6 +1,5 @@
 package gloomhavenPrototype;
 
-import java.util.Scanner;
 
 public class Enemy{
     String name = "";
@@ -35,81 +34,4 @@ public class Enemy{
 	public void setEMapPosY(int newMapPosY) {
 		this.mapPosY = newMapPosY;
 	}
-}
-/**
- * @param currentMap
- */
-public void move(Gridmap currentMap[][]) {
-	boolean madeMove = false;
-	Scanner moveCommand = new Scanner(System.in);
-	System.out.println("Current Enemy position: " + mapPosX + ", " + mapPosY);
-
-	while(!madeMove) {
-		int command = moveCommand.nextInt();
-		switch(command) {
-		case 1: //Moves Enemy Up Left 
-			if(mapPosY % 2 == 0 && mapPosX > 0 && mapPosY > 0) {
-				this.setEMapPosX(mapPosX -1);
-				this.setEMapPosY(mapPosY -1);
-				madeMove = true;
-			} 
-			else if (mapPosY > 0) {
-				this.setEMapPosY(mapPosY - 1);
-				madeMove = true;
-			}
-			System.out.println("Enemy moved to position: " + mapPosX + ", " + mapPosY);
-			break;
-		case 2: // Moves Enemy Up Right
-			if(mapPosY % 2 == 0 && mapPosY > 0) {
-				this.setEMapPosY(this.getMapPosY()-1);
-				madeMove = true;
-			}
-			else if(mapPosY > 0 && mapPosX < currentMap.length-1) {
-				this.setEMapPosY(this.getMapPosY()-1);
-				this.setEMapPosX(this.getMapPosX()+1);
-				madeMove = true;
-			}
-			System.out.println("Enemy moved to position: " + mapPosX + ", " + mapPosY);
-			break;
-		case 3: //Moves Enemy Left 
-			if(mapPosX > 0) {
-				this.setEMapPosX(this.getMapPosX()-1);
-				madeMove = true;
-			}
-			System.out.println("Enemy moved to position: " + mapPosX + ", " + mapPosY);
-			break;
-		case 4: //Moves Enemy Right
-			if(mapPosX < currentMap.length-1) {
-				this.setEMapPosX(this.getMapPosX()+1);
-				madeMove = true;
-			}
-			System.out.println("Enemy moved to position: " + mapPosX + ", " + mapPosY);
-			break;
-		case 5: //Moves Enemy Down Left
-			if(mapPosY % 2 == 0 && mapPosX > 0 && mapPosY < currentMap[0].length-1) {
-				this.setEMapPosX(this.getMapPosX()-1);
-				this.setEMapPosY(this.getMapPosY()+1);
-				madeMove = true;
-			}
-			else if(mapPosY < currentMap[0].length-1) {
-				this.setEMapPosY(this.getMapPosY()+1);
-				madeMove = true;
-			}
-			System.out.println("Enemy moved to position: " + mapPosX + ", " + mapPosY);
-			break;
-		case '6': //Moves Enemy Down Right
-			if(mapPosY % 2 == 0 && mapPosY < currentMap[0].length-1) {
-				this.setEMapPosY(this.getMapPosY()+1);
-				madeMove = true;
-			}
-			else if(mapPosX < currentMap[0].length-1 && mapPosY < currentMap[0].length-1) {
-				this.setEMapPosX(this.getMapPosX()+1);
-				this.setEMapPosY(this.getMapPosY()+1);
-				madeMove = true;
-			}
-			System.out.println("Enemy moved to position: " + mapPosX + ", " + mapPosY);
-			break;
-		}
-	}
-  }
 }
