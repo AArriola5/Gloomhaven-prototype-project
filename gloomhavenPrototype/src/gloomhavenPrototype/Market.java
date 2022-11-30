4a994bb0088ef04e63bb54b011746a47aa56ce1f
Goofus <jbellido158@gmail.com>
2022-11-30 11:15:16 -0800

    Implemented sell/buy & prosperity

--- gloomhavenPrototype/src/gloomhavenPrototype/Market.java
+++ gloomhavenPrototype/src/gloomhavenPrototype/Market.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 public final class Market {
 	private static HashMap<String, Items> MarketData = new HashMap<>();
 	public static ArrayList<Items>[] levels = new ArrayList[4]; //array of lists for each item level
-	
+
 	public Market() {
         for (int i = 0; i < 4; i++) {
         	levels[i] = new ArrayList<Items>();
@@ -16,13 +16,41 @@ public final class Market {
 	}
 	
 	//Helper function for adding an Item
-	public static void addItem(Items toAdd) {
+	private static void addItem(Items toAdd) {
 		MarketData.put(toAdd.getItemName(), toAdd);
 		levels[toAdd.getItemLevel() - 1].add(toAdd);
 	}
+	//Helper for removing item
+	private static void removeItem(Items toRemove) {
+		for (int i = 0; i < levels[toRemove.getItemLevel() - 1].size(); i++ ) {
+			Items curItem = levels[toRemove.getItemLevel() - 1].get(i);
+			if (curItem.getItemName() == toRemove.getItemName()) {
+				levels[toRemove.getItemLevel() - 1].remove(i);
+			}
+		}
+	}
 	
-	public static void OnPurchase (Player player, Items toBuy) {
+	public static void Purchase(Player player, Items toBuy) {
 		//Money validations.
+		if (player.getGold() < toBuy.getPrice()) { return; }
+		player.addToInventory(toBuy);
+		removeItem(toBuy);
+	}
+	
+	public static void Sell(Player player, Items toSell, int index) {
+		player.removeFromInventory(toSell,index);
+		player.addGold(toSell.getPrice() / 2);
+		addItem(toSell);
+	}
+
+	public static ArrayList<Items> getItemsForSale() {
+		ArrayList<Items> ret = new ArrayList<Items>();
+		for (int i = 0; i < City.getProsperityLevel(); i++ ) {
+			for (Items item : levels[i]) {
+				ret.add(item);
+			}
+		}
+		return ret;
 	}
 	
 	//Creates some items for the market.
@@ -48,7 +76,7 @@ public final class Market {
 			ItemUseType.REUSABLE,
 			ItemEquipType.ONEHAND,
 			//Called then item is used. 'player' is the one who used the item
-			(Items thisItem, Player player) -> {	
+			(Items thisItem, Player player) -> {
 				Events.Add("OnPlayerDamage", "HeaterShieldDamage", (Entity[] ents) -> {
 					thisItem.itemUseCount++;
 					if (thisItem.itemUseCount >= 3) {
