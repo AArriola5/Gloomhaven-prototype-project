a52ac69fa48e9d4446e6337a2a36bf02ec2859a9
Goofus <jbellido158@gmail.com>
2022-12-02 00:08:46 -0800

    Created City with View and test cases class

---
+++ gloomhavenPrototype/src/gloomhavenPrototype/City.java
@@ -0,0 +1,44 @@
+package gloomhavenPrototype;
+
+
+public final class City {
+	private static int prosperityLevel = 1; //Posperity's max level is 4. This is for the sake of time.
+	private static int prosperityProg = 0; //Progress counter
+	private static int churchBank = 0; //Amount of money given to the church
+	
+	//Prosperity accessors
+	public static int getProsperityLevel() {
+		return prosperityLevel;
+	}
+	public static int getProsperityXP() {
+		return prosperityProg;
+	}
+	public static int getDonations() {
+		return churchBank;
+	}
+	//When the Player decides to rest
+	public static void rest(Player player) {
+		player.setHealth(Math.min(player.getHealth() + 2, 10));
+	}
+	
+	//Helper function for giving a prosperity point to city
+	private static void giveProsperity() {
+		prosperityProg++;
+		if (prosperityProg == (4 + prosperityLevel - 1)) {
+			prosperityLevel++;
+			prosperityProg = 0;
+		}
+	}
+	//Player will donate 50 gold to the church 
+	public static void donate(Player player) {
+		if (player.getGold() < 50 || prosperityLevel == 4) {return;}
+		player.takeGold(50);
+		churchBank = churchBank + 50;
+		
+		//Every 100 gold city gains a point
+		if (churchBank >= 100) {
+			giveProsperity();
+			churchBank = churchBank - 100;
+		}
+	}
+}
