af8143d7384f444b3b41c5661800305e18f1d0c2
adria <adria@ADRIAN-DESKTOP>
2022-12-01 17:19:15 -0800

    Changed Driver to include a occupation for gridmap (Not fully implemented) Fixed compilation issue with EnemyCards Moved Player print messages to PlayerView Changed Gridmap to include set and get for occupation.

--- gloomhavenPrototype/src/gloomhavenPrototype/Driver.java
+++ gloomhavenPrototype/src/gloomhavenPrototype/Driver.java
@@ -1,16 +1,52 @@
 package gloomhavenPrototype;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //For now just checks whether the Player can move around the hex grid map.
 //Need to properly change player to a list, but for now like this for testing
 public class Driver {
 	public static void main(String[] args) {
+		int turns = 2;
+		int mapX = 6;
+		int mapY = 6;
 		Gridmap testMap[][] = new Gridmap[6][6];
-		Player player1 = new Player("Tutorial Man");
 		
-		System.out.println("Testing map movement");
-		for(int i = 0; i < 5; i++) {
-			player1.move(testMap);			
+		//sets the map with default parameters, in this case so far just false occupation.
+		//There might be a better way of implementing this, but not sure.
+		for(int i = 0; i < mapX; i++) {
+			for(int j = 0; j < mapY; j++) {
+				testMap[i][j] = new Gridmap();
+			}
+		}
+		
+		ArrayList<Player> players = new ArrayList<>();
+		players.add(new Player ("John Test"));
+		players.add(new Player ("Bob Test", 3, 5));
+		
+		//This lets the map know if the space is occupied at the map position
+		for(int i = 0; i < players.size(); i++) {
+			int x = players.get(i).getMapPosX();
+			int y = players.get(i).getMapPosY();
+			testMap[x][y].setOccupied(true);
+		}
+		
+		//Menu list for which player is performing an action. 
+		for(int i = 0; i < players.size(); i++) {
+			System.out.println(players.get(i).getName() + "'s turn");
+			
+			for(int j = 0; j < players.get(i).getTurns(); j++) {
+				System.out.println("What action would you like to perform?\n"
+						+ "1: Move");
+				System.out.println("Turn: " + (j+1) + "/" + players.get(i).getTurns());
+				Scanner actionCommand = new Scanner(System.in);
+				int command = actionCommand.nextInt();
+				switch(command) {
+				case 1:	System.out.println("Testing map movement\n");
+						players.get(i).move(testMap);
+						break;
+				}
+			}
 		}
 	}
-	
 }
--- gloomhavenPrototype/src/gloomhavenPrototype/Player.java
+++ gloomhavenPrototype/src/gloomhavenPrototype/Player.java
@@ -7,6 +7,7 @@ public class Player {
 	int level = 1;
 	int health = 10;
 	int inventorySize = 5;
+	int turns = 2;
 	Items[] inventory = new Items[inventorySize];
 	int mapPosX = 2; //Position of player for hex grid.
 	int mapPosY = 5;
@@ -14,6 +15,11 @@ public class Player {
 	//Constructor to build a player, could add more variables later. Just the name for now
 	public Player(String name) {
 		this.name = name;
+	}
+	public Player(String name, int x, int y) {
+		this.name = name;
+		this.mapPosX = x;
+		this.mapPosY = y;
 	}
 	
 	public String getName() {
@@ -31,6 +37,9 @@ public class Player {
 	public int getMapPosY() {
 		return this.mapPosY;
 	}
+	public int getTurns() {
+		return this.turns;
+	}
 	public void setName(String newName) {
 		this.name = newName;
 	}
@@ -46,25 +55,21 @@ public class Player {
 	public void setMapPosY(int newMapPosY) {
 		this.mapPosY = newMapPosY;
 	}
+	public void setTurns(int newTurns) {
+		this.turns = newTurns;
+	}
 	
 	//Method to print out user's inventory, not yet implemented
 	public void showInventory() {
-		for (int i = 0; i < inventorySize; i++) {
-			System.out.println(inventory[i].toString());
-		}
+		PlayerView.printPlayerInventory(inventory, inventorySize);
 	}
+	
 	//Method to move the player around the map, Need to implement whether the tile is occupied by another enemy or player.
 	public void move(Gridmap currentMap[][]) {
 		boolean madeMove = false; //Boolean to check whether the player made a move and get them out of the loop
 		Scanner moveCommand = new Scanner(System.in);
-		System.out.println("Current position: " + mapPosX + ", " + mapPosY);
-		System.out.println("Where do you want to move?" + '\n' + 
-				"1: Up Left" + '\n' +
-				"2: Up Right" + '\n' +
-				"3: Left" + '\n' +
-				"4: Right" + '\n' +
-				"5: Down Left" + '\n' +
-				"6: Down Right");
+		PlayerView.printPlayerLoc(mapPosX, mapPosY);
+		PlayerView.printMovementChoices();
 		
 		while(!madeMove) {
 			int command = moveCommand.nextInt();
@@ -79,7 +84,10 @@ public class Player {
 					this.setMapPosY(mapPosY - 1);
 					madeMove = true;
 				}
-				System.out.println("moved to position: " + mapPosX + ", " + mapPosY);
+				else {
+					PlayerView.printErrorMovement(mapPosX, mapPosY);
+				}
+				PlayerView.printPlayerNewLoc(mapPosX, mapPosY);
 				break;
 			case 2: // Moves player Up Right
 				if(mapPosY % 2 == 0 && mapPosY > 0) {
@@ -91,21 +99,30 @@ public class Player {
 					this.setMapPosX(this.getMapPosX()+1);
 					madeMove = true;
 				}
-				System.out.println("moved to position: " + mapPosX + ", " + mapPosY);
+				else {
+					PlayerView.printErrorMovement(mapPosX, mapPosY);
+				}
+				PlayerView.printPlayerNewLoc(mapPosX, mapPosY);
 				break;
 			case 3: //Moves Player Left / Doesn't need even or odd check
 				if(mapPosX > 0) {
 					this.setMapPosX(this.getMapPosX()-1);
 					madeMove = true;
 				}
-				System.out.println("moved to position: " + mapPosX + ", " + mapPosY);
+				else {
+					PlayerView.printErrorMovement(mapPosX, mapPosY);
+				}
+				PlayerView.printPlayerNewLoc(mapPosX, mapPosY);
 				break;
 			case 4: //Moves Player Right
 				if(mapPosX < currentMap.length-1) {
 					this.setMapPosX(this.getMapPosX()+1);
 					madeMove = true;
 				}
-				System.out.println("moved to position: " + mapPosX + ", " + mapPosY);
+				else {
+					PlayerView.printErrorMovement(mapPosX, mapPosY);
+				}
+				PlayerView.printPlayerNewLoc(mapPosX, mapPosY);
 				break;
 			case 5: //Moves Player Down Left
 				if(mapPosY % 2 == 0 && mapPosX > 0 && mapPosY < currentMap[0].length-1) {
@@ -117,9 +134,12 @@ public class Player {
 					this.setMapPosY(this.getMapPosY()+1);
 					madeMove = true;
 				}
-				System.out.println("moved to position: " + mapPosX + ", " + mapPosY);
+				else {
+					PlayerView.printErrorMovement(mapPosX, mapPosY);
+				}
+				PlayerView.printPlayerNewLoc(mapPosX, mapPosY);
 				break;
-			case '6': //Moves Player Down Right
+			case 6: //Moves Player Down Right
 				if(mapPosY % 2 == 0 && mapPosY < currentMap[0].length-1) {
 					this.setMapPosY(this.getMapPosY()+1);
 					madeMove = true;
@@ -129,7 +149,10 @@ public class Player {
 					this.setMapPosY(this.getMapPosY()+1);
 					madeMove = true;
 				}
-				System.out.println("moved to position: " + mapPosX + ", " + mapPosY);
+				else {
+					PlayerView.printErrorMovement(mapPosX, mapPosY);
+				}
+				PlayerView.printPlayerNewLoc(mapPosX, mapPosY);
 				break;
 			}
 		}
